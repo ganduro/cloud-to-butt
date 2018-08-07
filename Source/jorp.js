2c58ee5bba2ec24fc2523b30e7f9ca13253001e8
ganduro <ganduro@gmail.com>
2018-08-06 17:22:34 -1000

    Add files via upload

---
+++ Source/jorp.js
@@ -0,0 +1,49 @@
+//credit goes to Steven Frank of Cloud to Butt (https://github.com/panicsteve/cloud-to-butt/)
+
+walk(document.body);
+
+function walk(node)  
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1: 
+		case 9:  
+		case 11: 
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling; 
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: 
+			handleText(node);
+			break;
+	}
+}
+
+function getJorp() {
+
+	var jorp = ["Jorp", "Jordy-P", "J. Peeperson", "Jordgie Porgie", "the oldest man named Jordan"]
+	
+	var index = Math.floor(Math.random() * jorp.length)
+	return jorp[index]
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bJordan Peterson\b/g, getJorp());
+	v = v.replace(/\bJordan B Peterson\b/g, getJorp());
+	v = v.replace(/\bJordan Bernt Peterson\b/g, getJorp());
+	
+	textNode.nodeValue = v;
+}
